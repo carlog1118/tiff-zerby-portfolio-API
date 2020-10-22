2ec715f77178692134fe3453f84bd894ee882ab6
Carlo Giustozzi <carlog1118@gmail.com>
2020-10-21 23:12:04 -0400

    implemented POST /testimonials

--- src/Testimonials/testimonials-router.js
+++ src/Testimonials/testimonials-router.js
@@ -2,16 +2,33 @@ const express = require("express");
 const testimonialsRouter = express.Router();
 const TestimonialsService = require("./testimonials-service");
 const errorHandler = require("../error-handler");
-//const bodyParser = express.json();
+const jsonParser = express.json();
 
-testimonialsRouter.route("/api/testimonials").get((req, res) => {
-  const knexInstance = req.app.get("db");
-  TestimonialsService.getAllTestimonials(knexInstance)
+testimonialsRouter
+  .route("/api/testimonials")
+  .get((req, res) => {
+    const knexInstance = req.app.get("db");
+    TestimonialsService.getAllTestimonials(knexInstance)
     .then((testimonials) => {
       res.json(testimonials);
     })
     .catch(errorHandler);
-});
-//.post((req, res) => {/*code*/})
+    })
+  .post(jsonParser,(req, res) => {
+    const { client, quote, author } = req.body
+    const newTest = { client, quote, author }
+    const knexInstance = req.app.get("db");
+    TestimonialsService.insertTest(
+      knexInstance,
+      newTest
+    )
+      .then(test => {
+        res
+          .status(201)
+          .location(`/testimonials/${test.id}`)
+          .json(test)
+      })
+      .catch(errorHandler)
+  })
 
 module.exports = testimonialsRouter;
--- src/Testimonials/testimonials-service.js
+++ src/Testimonials/testimonials-service.js
@@ -2,6 +2,15 @@ const TestimonialsService = {
   getAllTestimonials(knex) {
     return knex.select("*").from("testimonials");
   },
+  insertTest(knex, newTest) {
+    return knex
+      .insert(newTest)
+      .into('testimonials')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  }
 };
 
 module.exports = TestimonialsService;
