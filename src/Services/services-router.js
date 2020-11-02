715ea004ae329f2da645c3360ccb1eaa3562e663
Carlo Giustozzi <carlog1118@gmail.com>
2020-11-02 15:53:57 -0500

    implemented DELETE /services/:id endpoint

--- src/Services/services-router.js
+++ src/Services/services-router.js
@@ -4,14 +4,16 @@ const ServicesService = require("./services-service");
 const errorHandler = require("../error-handler");
 const jsonParser = express.json();
 
-servicesRouter.route("/api/services").get((req, res) => {
-  const knexInstance = req.app.get("db");
-  ServicesService.getAllServices(knexInstance)
-    .then((services) => {
-      res.json(services);
-    })
-    .catch(errorHandler);
-})
+servicesRouter
+  .route("/api/services")
+  .get((req, res) => {
+    const knexInstance = req.app.get("db");
+    ServicesService.getAllServices(knexInstance)
+      .then((services) => {
+        res.json(services);
+      })
+      .catch(errorHandler);
+  })
   .post(jsonParser, (req, res) => {
     const { name, description } = req.body;
     const newServ = { name, description };
@@ -21,6 +23,45 @@ servicesRouter.route("/api/services").get((req, res) => {
         res.status(201).location(`/testimonials/${serv.id}`).json(serv);
       })
       .catch(errorHandler);
+  });
+
+servicesRouter.route("/api/services/:id")
+  .get((req, res) => {
+    const knexInstance = req.app.get("db");
+    const id = req.params.id;
+    ServicesService.getById(knexInstance, id)
+      .then((serv) => {
+        res.json(serv)
+      })
+      .catch(errorHandler);
   })
+  .delete((req, res) => {
+    const knexInstance = req.app.get("db");
+    const id = req.params.id;
+    ServicesService.deleteServ(knexInstance, id)
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(errorHandler);
+  })
+  /*.patch(jsonParser,(req, res) => {
+    const knexInstance = req.app.get("db")
+    const { client, quote, author } = req.body;
+    const updatedTest = { client, quote, author }
+
+    const numberOfValues = Object.values(updatedTest).filter(Boolean).length;
+    if(numberOfValues === 0) {
+      return res.status(400).json({
+        error: {
+          message: `request body must contain at least one of the fields.`
+        }
+      });
+    };
+
+    TestimonialsService.updateTest(knexInstance, req.params.id, updatedTest)
+      .then(res.status(204).end())
+      .catch(errorHandler)
+  })*/
+
 
 module.exports = servicesRouter;
